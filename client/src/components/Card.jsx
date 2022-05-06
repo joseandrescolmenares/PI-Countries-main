dfb04a1b705bbedb607ab8c9c17a4ef740c53f91
jose <joseandrescolmenares02@gmail.com>
2022-05-05 22:34:10 -0300

    detalles

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,10 +4,11 @@ import style from "./css/Card.module.css";
 const Card = ({ name, flag, continent }) => {
   return (
     <div className={style.card}>
-      <img className={style.flag} src={flag} alt="bandera" />
+      
       <div>
+      <img className={style.flag} src={flag} alt="bandera" />
         <h2 className={style.bla}>{continent}</h2>
-        <h1 className={style.bla}>{name}</h1>
+        <p className={style.bla}>{name}</p>
       </div>
     </div>
   );
